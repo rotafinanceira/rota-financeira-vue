f4f162c2898a2cf580efdc4ec7ef48efdc6ddf06
attilamonteiro <atmaload@gmail.com>
2025-02-16 22:31:00 -0300

    fix(SignUpStep2, SignUpStep3): update logo structure and styling for better alignment and visibility

--- src/pages/SignUp/SignUpStep2.vue
+++ src/pages/SignUp/SignUpStep2.vue
@@ -5,8 +5,11 @@
         <div @click="navigateBack" class="return-button">
           <q-img :src="backArrow" />
         </div>
-        <div class="logo-container">
-          <q-img :src="logo" class="logo" />
+        <div class="logo">
+          <img :src="logo" alt="Logo" />
+          <div class="logo-text">
+            <span class="bold-text">Rota</span>Financeira
+          </div>
         </div>
       </div>
       <SignUpTitleStepper :step="2" />
@@ -43,7 +46,7 @@
 import { ref } from 'vue';
 import { useRouter } from 'vue-router';
 import { useRegisterStore } from '@/store/registerStore';
-import logo from '@/assets/logolight.svg';
+import logo from '@/assets/logoRF.svg';
 import backArrow from '@/assets/backarrow.svg';
 import SignInUpFooter from '@/components/SignInUpFooter.vue';
 import SignUpTitleStepper from '@/components/SignUpTitleStepper.vue';
@@ -104,12 +107,17 @@ const handleSubmit = () => {
   align-items: center;
   margin-top: 24px;
   padding: 6px 20px;
+  position: relative; /* Ensure relative positioning for alignment */
 }
 
 .return-button {
   width: 24px;
   height: 24px;
   cursor: pointer;
+  position: absolute;
+  left: 20px; /* Align with the logo */
+  top: 50%; /* Center vertically */
+  transform: translateY(-50%); /* Center vertically */
 }
 
 .logo-container {
@@ -120,7 +128,37 @@ const handleSubmit = () => {
 }
 
 .logo {
-  width: 165px;
+  display: flex;
+  align-items: center;
+  gap: 10px;
+  position: absolute;
+  left: 85px; /* Adjusted to add 65px space from return-button */
+}
+
+.logo-text {
+  display: flex;
+  align-items: center; /* Center vertically */
+  width: 128.344px;
+  height: 13.558px;
+  flex-shrink: 0;
+  color: var(--Cores-Primria-800, #245017);
+  font-family: Inter;
+  font-size: 18.346px;
+  font-style: italic;
+  font-weight: 400;
+  line-height: 13.76px; /* 75% */
+  letter-spacing: -0.202px;
+}
+
+.bold-text {
+  font-weight: 700;
+}
+
+.logo img {
+  width: 19.734px;
+  height: 18px;
+  flex-shrink: 0;
+  fill: var(--Cores-Primria-200, #8ce95f);
 }
 
 .container-content {
@@ -139,5 +177,11 @@ const handleSubmit = () => {
   flex-direction: column;
   gap: 20px;
   margin-bottom: 32px;
+}
+
+.error {
+  color: red;
+  font-size: 14px;
+  margin-top: 5px;
 }
 </style>
