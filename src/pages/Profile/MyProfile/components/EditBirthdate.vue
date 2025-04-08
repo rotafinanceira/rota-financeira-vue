a3b180b5d74a7a5286e11c5b330fce37cbc41c15
Ricardo Magalhães <ricardogleiss@hotmail.com>
2025-04-08 01:14:47 -0300

    feat(MyProfile): finish EditBirthdate page design

--- src/pages/Profile/MyProfile/components/EditBirthdate.vue
+++ src/pages/Profile/MyProfile/components/EditBirthdate.vue
@@ -1,52 +1,34 @@
 <template>
-  <section class="edit-content">
-    <h3>Editar Data de Nascimento</h3>
-    <div class="edit-card">
-      <label>
-        Data de nascimento
-        <input type="date" v-model="birthdate" />
-      </label>
-      <button class="btn-salvar" @click="salvarEdicao">Salvar</button>
-    </div>
-  </section>
+  <EditField
+    title="Alterar data de nascimento"
+    description="Digite a data de nascimento correta em Data de Nascimento Atual e clique em salvar alterações."
+    :updateValue="updateBirthdate"
+  >
+    <label class="edit-label disabled">
+      Data de nascimento anterior
+        <input
+          class="edit-input disabled"
+          type="date"
+          value="1996-08-24"
+          disabled
+        />
+    </label>
+    <label class="edit-label">
+      Data de nascimento atual
+      <div class="input-wrapper">
+        <input class="edit-input" type="date" v-model="birthdate" />
+      </div>
+    </label>
+  </EditField>
 </template>
 
 <script setup lang="ts">
 import { ref } from 'vue';
+import EditField from './EditField.vue';
 
 const birthdate = ref('');
 
-function salvarEdicao() {
-  console.log('Salvar Data de Nascimento');
+function updateBirthdate() {
+  console.log('Update birthdate');
 }
 </script>
-
-<style scoped>
-.edit-content {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  padding: 1rem;
-}
-
-.edit-card {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  padding: 1rem;
-  background-color: #fff;
-  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
-  border-radius: 8px;
-  margin-bottom: 1rem;
-}
-
-.btn-salvar {
-  margin: 1rem;
-  padding: 0.75rem 1rem;
-  background-color: #4caf50;
-  color: #fff;
-  border: none;
-  font-weight: bold;
-  cursor: pointer;
-}
-</style>
