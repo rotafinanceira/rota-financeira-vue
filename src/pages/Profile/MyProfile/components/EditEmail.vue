726c95ccd952c4c6d44dbef4be387080223cfb67
Ricardo Magalhães <ricardogleiss@hotmail.com>
2025-04-08 01:14:26 -0300

    feat(MyProfile): finish EditEmail page design

--- src/pages/Profile/MyProfile/components/EditEmail.vue
+++ src/pages/Profile/MyProfile/components/EditEmail.vue
@@ -1,52 +1,34 @@
 <template>
-  <section class="edit-content">
-    <h3>Editar E-mail</h3>
-    <div class="edit-card">
-      <label>
-        E-mail
-        <input type="email" v-model="email" />
-      </label>
-      <button class="btn-salvar" @click="salvarEdicao">Salvar</button>
-    </div>
-  </section>
+  <EditField
+    title="Alterar e-mail"
+    description="Digite um novo e-mail em E-mail atual e clique em salvar alterações."
+    :updateValue="updateEmail"
+  >
+    <label class="edit-label disabled">
+      E-mail anterior
+      <input
+        class="edit-input disabled"
+        type="email"
+        value="ana.maria@gmail.com"
+        disabled
+      />
+    </label>
+    <label class="edit-label">
+      E-mail atual
+      <div class="input-wrapper">
+        <input class="edit-input" type="text" v-model="email" />
+      </div>
+    </label>
+  </EditField>
 </template>
 
 <script setup lang="ts">
 import { ref } from 'vue';
+import EditField from './EditField.vue';
 
 const email = ref('');
 
-function salvarEdicao() {
-  console.log('Salvar E-mail');
+function updateEmail() {
+  console.log('Update E-mail');
 }
 </script>
-
-<style scoped>
-.edit-content {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  padding: 1rem;
-}
-
-.edit-card {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  padding: 1rem;
-  background-color: #fff;
-  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
-  border-radius: 8px;
-  margin-bottom: 1rem;
-}
-
-.btn-salvar {
-  margin: 1rem;
-  padding: 0.75rem 1rem;
-  background-color: #4caf50;
-  color: #fff;
-  border: none;
-  font-weight: bold;
-  cursor: pointer;
-}
-</style>
