db0d97b7f3929e34892e01651e550a5f809b6396
Luciano Martins <luciano.martins@quay.com.br>
2025-04-22 10:09:41 -0300

    Add images

--- src/pages/NotificationPage.vue
+++ src/pages/NotificationPage.vue
@@ -1,86 +1,88 @@
 <template>
-    <q-page class="q-pa-md" style="background-color: #F5F5F5;">
-        <q-toolbar>
-            <q-btn
-                flat
-                round
-                icon="chevron_left" 
-                :to="{path: 'home'}" 
-            />
-            <q-toolbar-title class="toolbar-title">Notificações</q-toolbar-title>
-        </q-toolbar>
-        
-        <q-list separator class="q-mt-md">
-            <div class="notification-card">
-                <q-item
-                    v-for="notification in notifications" :key="notification.id"
-                    class="q-mb-md"
-                >
-                    <q-card
-                        :class="{
-                            'notification-card--priority-alta': notification.priority === 'alta',
-                            'notification-card--priority-baixa': notification.priority === 'baixa'
+    <div>
+        <AppHeader title="Notificações" path="/home" />
+        <q-page class="q-pa-md notification-page">
+            <q-item v-for="notification in notifications" :key="notification.id" >
+                <q-card 
+                    :class="{
+                        'notification-card--priority-alta': notification.priority === 'alta', 
+                        'notification-card--priority-baixa': notification.priority === 'baixa' 
                         }"
-                        class="full-width"
                     >
-                        <q-card-section horizontal> 
-                            <component
-                                class="col-2"
-                                :is="notification.icon"
-                                size="2.5em"
-                                color="primary" 
-                            />
-                            <q-card-section 
-                                class="col-10"
-                                style="padding-left: 0px; padding-right: 0px;"
-                            >
-                                <p>{{ notification.description }}</p>
-                            </q-card-section>
+                    <q-card-section horizontal class="notification-card-section">
+                        <div class="notification-img">
+                            <q-img :src="getNotificationIcon(notification.name)" alt="Notification Icon" />
+                        </div>
+                        <q-card-section>
+                            <span class="notification-letter">{{ notification.description }}</span>
                         </q-card-section>
-                    </q-card>
-                </q-item>
-            </div>
-        </q-list>
+                    </q-card-section>
+                </q-card>
+            </q-item>
+        </q-page>
         <AppFooter />
-    </q-page>
+    </div>
 </template>
 <script setup="ts">
 
+import AppHeader from 'components/HeaderBarNotification.vue';
 import AppFooter from 'components/AppFooter.vue';
-import carro from '@/assets/carroNotification.svg';
+import carro from '@/assets/CarroPerfil.svg';
+import bateria from '@/assets/Bateria.svg';
+import alinhamento from '@/assets/Alinhamento.svg';
+import oleo from '@/assets/Oleo.svg';
 
 const notifications = [
     {
         id: 1,
         description: 'Atenção! A vida útil da sua bateria está chegando ao fim, marque sua revisão.',
-        icon: carro,
+        name: 'Bateria',
         priority: 'alta'
     },
     {
         id: 2,
-        description: 'Atenção! A troca de óleo de seu veículo está atrasada. Mantenha seu veículo bem cuidado!',
-        icon: 'alarm',
+        description: 'Atenção! A troca de óleo do seu veiculo esta atrasada. Mantenha seu veículo bem cuidado!',
+        name: 'Óleo',
         priority: 'baixa'
     },
     {
         id: 3,
-        description: 'Atenção! Você trocou os pneus e precisa fazer o seu alinhamento e balanceamento!',
-        icon: 'watch',
+        description: 'Atenção! Você trocou os pneus e precisa fazer o seu alinhamento e balanceamento.',
+        name: 'Alinhamento',
         priority: 'alta'
-    },
+    },    
     {
         id: 4,
         description: 'Atenção! O prazo para fazer o alinhamento e balanceamento do seu carro expirou.',
-        icon: 'timer',
+        name: 'Alinhamento',
         priority: 'baixa'
     },
     {
         id: 5,
         description: 'Não se esqueça de atualizar a sua quilometragem!',
-        icon: 'devices',
+        name: 'Alinhamento',
+        priority: 'baixa'
+    },
+    {
+        id: 6,
+        description: 'Está chegando a hora da revisão de fábrica do seu carro! 🚗Programe-se com antecedência para não ser pego de surpresa!',
+        name: 'Carro',
         priority: 'alta'
     },
 ];
+
+
+const notificationIcons = {    
+    'Bateria': bateria,
+    'Carro': carro,
+    'Alinhamento': alinhamento,
+    'Óleo': oleo,
+};
+
+function getNotificationIcon(name) {
+    return notificationIcons[name] || null;
+}
+
 
 // function getNotification() {
 //     const notification = api.get('/notification')
@@ -95,24 +97,46 @@ const notifications = [
 </script>
 
 <style scoped>
+
+.notification-page {
+    background-color: #F5F5F5;
+    font-size: 14px;
+    display: flex;
+    flex-direction: column;
+    min-height: 80px; /* Ajuste este valor conforme necessário */
+    min-width: 100%; /* Para que todos os cards ocupem a largura total do q-item */
+}
+
+.notification-letter {
+    color: var(--Botes-Preenchidos-Ativado-Texto, #2b5e16);
+}
+
 .notification-card--priority-alta {
-  border-top: 4px solid red;
+    border-top: 4px solid red;
 }
 
 .notification-card--priority-baixa {
-  border-top: 4px solid green;
+    border-top: 4px solid green;
 }
 
-p {
-  margin: 0;
-  font-size: 1.2em;
+.notification-icon {    
+    font-size: 1.5em;
 }
 
-.toolbar-title {
-  font-size: 1.5em;
-  font-weight: bold;
-  color: #2B5E16;
-  text-align: center;
-
+.notification-card-section {
+    display: flex; 
+    align-items: center;
 }
+
+.notification-description {    
+    font-size: 1.5em;
+}
+
+.notification-img {
+    width: 28px;
+    height: 28px;
+    margin-left: 10px;
+    background-color: #EFF3F5;
+}
+
 </style>
