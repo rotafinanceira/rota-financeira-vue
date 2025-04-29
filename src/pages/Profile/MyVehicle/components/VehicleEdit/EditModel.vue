1ddcf59ec01c4aca8f639503eeed6c752044e4e3
Ricardo Magalhães <ricardogleiss@hotmail.com>
2025-04-29 01:34:23 -0300

    feat(MyVehicle): create edit car pages view

---
+++ src/pages/Profile/MyVehicle/components/VehicleEdit/EditModel.vue
@@ -0,0 +1,34 @@
+<template>
+  <EditField
+    title="Modelo"
+    description="Digite o modelo completo correto em Modelo Atual e clique em salvar alterações."
+    :updateValue="updateModel"
+  >
+    <label class="edit__label disabled">
+      Modelo anterior
+      <input
+        class="edit__input disabled"
+        type="text"
+        value="Corolla"
+        disabled
+      />
+    </label>
+    <label class="edit__label">
+      Modelo atual
+      <div class="input-wrapper">
+        <input class="edit__input" type="text" v-model="model" />
+      </div>
+    </label>
+  </EditField>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+import EditField from '@/pages/Profile/components/EditField.vue';
+
+const model = ref('');
+
+function updateModel() {
+  console.log('Update model');
+}
+</script>
