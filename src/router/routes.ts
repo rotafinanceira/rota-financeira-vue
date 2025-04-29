22dd8329007ab57e4072472e9f430ede9aa6485e
Ricardo Magalhães <ricardogleiss@hotmail.com>
2025-04-29 01:34:41 -0300

    feat(MyVehicle): configure edit car routes

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -97,9 +97,25 @@ const routes: RouteRecordRaw[] = [
         component: () => import('@/pages/Home/HomePage.vue'),
       },
       {
-        path: 'maintenance',
-        component: () => import('@/pages/Maintenance/MaintenancePage.vue'),
+        path: 'vehicle/edit-model',
+        component: () => import('@/pages/Profile/MyVehicle/components/VehicleEdit/EditModel.vue'),
       },
+      {
+        path: 'vehicle/edit-brand',
+        component: () => import('@/pages/Profile/MyVehicle/components/VehicleEdit/EditBrand.vue'),
+      },
+      {
+        path: 'vehicle/edit-year',
+        component: () => import('@/pages/Profile/MyVehicle/components/VehicleEdit/EditYear.vue'),
+      },
+      {
+        path: 'vehicle/edit-plate',
+        component: () => import('@/pages/Profile/MyVehicle/components/VehicleEdit/EditPlate.vue'),
+      },
+      {
+        path: 'vehicle/edit-color',
+        component: () => import('@/pages/Profile/MyVehicle/components/VehicleEdit/EditColor.vue'),
+      }
     ],
   },
 
