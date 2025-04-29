05c9365cd6f6a26897c7e25fe80093abcaaf31cf
Ricardo Magalhães <ricardogleiss@hotmail.com>
2025-04-29 01:33:32 -0300

    refactor: move css variable to class scope

--- src/pages/Profile/components/EditField.vue
+++ src/pages/Profile/components/EditField.vue
@@ -22,9 +22,9 @@ const { title, description, updateValue } = defineProps<{
 
 <style scoped lang="scss">
 .edit {
-  --footer-bar-size: 72px;
 
   &__content {
+    --footer-bar-size: 72px;
     display: flex;
     flex-direction: column;
     min-height: calc(100svh - var(--footer-bar-size));
