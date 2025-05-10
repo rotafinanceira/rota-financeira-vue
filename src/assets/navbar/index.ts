97658e88ca970ede075619c0b645fec853a879f7
Ricardo Magalhães <ricardogleiss@hotmail.com>
2025-05-10 08:05:12 -0300

    feat: add updated navbar icons

---
+++ src/assets/navbar/index.ts
@@ -0,0 +1,30 @@
+import homeEnabled from './home-enabled.svg';
+import homeDisabled from './home-disabled.svg';
+
+import historyEnabled from './history-enabled.svg';
+import historyDisabled from './history-disabled.svg';
+
+import maintenanceEnabled from './maintenance-enabled.svg';
+import maintenanceDisabled from './maintenance-disabled.svg';
+
+import profileEnabled from './profile-enabled.svg';
+import profileDisabled from './profile-disabled.svg';
+
+export const navbar = {
+  home: {
+    enabled: homeEnabled,
+    disabled: homeDisabled,
+  },
+  history: {
+    enabled: historyEnabled,
+    disabled: historyDisabled,
+  },
+  maintenance: {
+    enabled: maintenanceEnabled,
+    disabled: maintenanceDisabled,
+  },
+  profile: {
+    enabled: profileEnabled,
+    disabled: profileDisabled,
+  },
+};
