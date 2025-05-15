9734549fe210295c0b546299f18b5531b2b39c4a
Ricardo Magalhães <ricardogleiss@hotmail.com>
2025-05-15 19:01:41 -0300

    chore: comment store configurations
    commented store configurations so it doesn't throw errors (the store is not being currently used)

--- src/store/exemplo.ts
+++ src/store/exemplo.ts
@@ -1,20 +1,24 @@
-import { defineStore } from 'pinia';
-import { api } from 'src/boot/axios';
+// import { defineStore } from 'pinia';
+// import { api } from 'src/boot/axios';
 
-const exempleApi = import.meta.env.VITE_CREDIT_STATICDATA_API;
+// const exempleApi = import.meta.env.VITE_CREDIT_STATICDATA_API;
 
-export const useExemploStore = defineStore('ex', () => {
-  async function kkk() {
-    try {
-      const { data } = await api().get(`${ppp}/exemplo/all`);
-      return data;
-    } catch (e) {
-      return [];
-    }
-  }
+// export const useExemploStore = defineStore('ex', () => {
+//   async function kkk() {
+//     try {
+//       const { data } = await api().get(`${ppp}/exemplo/all`);
+//       return data;
+//     } catch (e) {
+//       return [];
+//     }
+//   }
 
 
-  return {
-    kkk,
-  };
-});
+//   return {
+//     kkk,
+//   };
+// });
+
+
+// Remover ao configurar arquivo
+export {}
