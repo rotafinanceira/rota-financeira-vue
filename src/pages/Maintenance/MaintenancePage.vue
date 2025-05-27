1f7f26e67a586e192a0153b4feeadb57f6457f57
Ricardo Magalhães <ricardogleiss@hotmail.com>
2025-05-27 01:42:20 -0300

    feat: add header and all maintenance page buttons

--- src/pages/Maintenance/MaintenancePage.vue
+++ src/pages/Maintenance/MaintenancePage.vue
@@ -2,15 +2,37 @@
   <AppHeader />
   <div class="maintenance">
     <div class="maintenance__wrapper">
-      <MaintenanceItem icon="wheel" title="Alinhamento" variant="card" />
-      <MaintenanceItem icon="wheel" title="Alinhamento" variant="card" />
+      <header class="maintenance__header">
+        <div class="flex">
+          <img :src="gearWrench" alt="" />
+          <h1 class="maintenance__title">Manutenções</h1>
+        </div>
+        <button
+          class="maintenance__settings"
+          type="button"
+          :onclick="openSettings"
+        >
+          <img :src="settings" alt="settings button" />
+        </button>
+      </header>
+      <MaintenanceItem
+        v-for="item in maintenanceItems"
+        :title="item.title"
+        :icon="item.icon"
+        variant="card"
+        :key="item.title"
+      />
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
+import { gearWrench, settings } from '@/assets';
 import AppHeader from '@/components/AppHeader.vue';
-import MaintenanceItem from '@/components/MaintenanceItem.vue';
+import MaintenanceItem, {
+  MaintenanceItemProps,
+} from '@/components/MaintenanceItem.vue';
+
 // import oilImage from '@/assets/agua.svg';
 // import manuIcon2 from '@/assets/manu2.svg';
 // import expiredIcon from '@/assets/manVen.svg';
@@ -23,250 +45,112 @@ import MaintenanceItem from '@/components/MaintenanceItem.vue';
 // import airConditionerImage from '@/assets/agua.svg';
 // import type { IMaintenanceItem } from '@/interfaces/IMaintenanceItem';
 
+const maintenanceItems: MaintenanceItemProps[] = [
+  {
+    title: 'Alinhamento e balanceamento',
+    icon: 'wheel',
+  },
+  {
+    title: 'Bateria',
+    icon: 'battery',
+  },
+  {
+    title: 'Filtro de ar-condicionado',
+    icon: 'airFilter',
+  },
+  {
+    title: 'Filtro de combustível',
+    icon: 'fuelFilter',
+  },
+  {
+    title: 'Nível de fluidos',
+    icon: 'fluidLevel',
+  },
+  {
+    title: 'Motor',
+    icon: 'engine',
+  },
+  {
+    title: 'Troca de óleo',
+    icon: 'oil',
+  },
+  {
+    title: 'Verificação de pneu',
+    icon: 'wheel',
+  },
+];
 
+const openSettings = () => {
+  console.log('open settings');
+};
 
-// const expiredMaintenances = ref<IMaintenanceItem[]>([
-//   {
-//     title: 'Alinhamento e balanceamento',
-//     date: 'Venceu dia 1 out. 2024',
-//     progress: '6/8',
-//     image: alignmentImage,
-//   },
-//   {
-//     title: 'Troca de óleo',
-//     date: 'Venceu dia 1 out. 2024',
-//     progress: '6/8',
-//     image: oilImage,
-//   },
-// ]);
+// ---
+// TODO: transferir para o shared types quando for criado
+type MaintenanceTypes =
+  | 'engine'
+  | 'battery'
+  | 'oil'
+  | 'air filter'
+  | 'wheel'
+  | 'alignment and balancing'
+  | 'fuel levels'
+  | 'fluid levels';
 
-// const nextMaintenances = ref<IMaintenanceItem[]>([
-//   {
-//     title: 'Alinhamento e balanceamento',
-//     date: 'Vence dia 12 out. 2025',
-//     progress: '6/8',
-//     image: alignmentImage,
-//   },
-//   {
-//     title: 'Troca de óleo',
-//     date: 'Vence dia 1 out. 2025',
-//     progress: '6/8',
-//     image: oilImage,
-//   },
-// ]);
+type MaintenanceStatus = 'unregistered' | 'pending' | 'expired';
+// ---
 
-// const fillStepsMaintenances = ref<IMaintenanceItem[]>([
-//   {
-//     title: 'Alinhamento e balanceamento',
-//     date: '',
-//     progress: '2/5',
-//     image: alignmentImage,
-//   },
-//   {
-//     title: 'Troca de óleo',
-//     date: '',
-//     progress: '3/5',
-//     image: oilImage,
-//   },
-// ]);
-
-// const completedMaintenances = ref<IMaintenanceItem[]>([
-//   {
-//     title: 'Alinhamento e balanceamento',
-//     date: '',
-//     progress: ' ',
-//     image: alignmentImage,
-//   },
-//   {
-//     title: 'Troca de óleo',
-//     date: '',
-//     progress: ' ',
-//     image: oilImage,
-//   },
-// ]);
-
+// WIP
+type MaintenanceItem = {
+  id: string;
+  maintenanceId: string;
+  maintenanceType: MaintenanceTypes;
+  currentKm: number;
+  status: MaintenanceStatus;
+  createdAt: Date;
+  pendingRegistration: number;
+};
 </script>
 
 <style lang="scss" scoped>
 .maintenance {
   min-height: calc(100svh - 130px);
-  background-color: #EFF3F5;
+  background-color: #eff3f5;
 
   &__wrapper {
+    width: calc(100% - 2.5rem);
+    margin-inline: auto;
     padding-top: 1.5rem;
     display: flex;
     flex-direction: column;
-    gap: 1.5rem;
-    width: calc(100% - 2.5rem);
-    margin-inline: auto;
+    gap: 1rem;
+  }
+
+  &__header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-bottom: 1rem;
+  }
+
+  &__title {
+    font-size: 1.25rem;
+    font-weight: 700;
+    font-family: 'Inter';
+    line-height: normal;
+    margin: 0;
+  }
+
+  &__settings {
+    height: 20px;
+    background: none;
+    border: none;
+    padding: 0;
   }
 }
-.maintenance-page {
-  max-width: 600px;
-  margin: 0 auto;
-  background-color: #f9f9f9;
-  margin-top: 1rem;
-}
 
-.maintenance-title {
+.flex {
   display: flex;
   align-items: center;
-  gap: 10px;
-  padding-left: 20px;
-  padding-top: 24px;
-  padding-bottom: 12px;
-  border-top: 100px;
-}
-
-.title-icon {
-  width: 24px;
-  height: 24px;
-}
-
-.title-text {
-  color: var(--Cores-Cinza-800, #33373c);
-  font-family: var(--Tipo-Familia-Headline, Raleway);
-  font-size: var(--Tipo-Tamanho-Xl, 20px);
-  font-style: normal;
-  font-weight: 700;
-  line-height: 120%;
-  letter-spacing: -0.4px;
-}
-
-.maintenance-section {
-  background-color: #ffffff;
-  margin-bottom: 1.5rem;
-  border-radius: 8px;
-  margin-left: 16px;
-  margin-right: 16px;
-}
-
-.separator {
-  width: 209px;
-  height: 1px;
-  background-color: #e0e0e0;
-  margin: 0 auto;
-}
-
-.section-header {
-  display: flex;
-  align-items: center;
-  margin-bottom: 0.75rem;
-  padding-left: 10px;
-}
-
-.section-icon-wrapper {
-  display: flex;
-  padding: 8px 5px;
-  align-items: center;
-  gap: 10px;
-  border-radius: 8px;
-}
-
-.section-icon {
-  margin-right: 0.5rem;
-}
-
-.section-image {
-  width: 20px;
-  height: 20px;
-}
-.section-title {
-  color: var(--Cores-Secundria-800, #223227);
-  font-family: var(--Tipo-Familia-Button, Inter);
-  font-size: var(--Tipo-Tamanho-Md, 16px);
-  font-style: normal;
-  font-weight: 600;
-  line-height: 120%;
-}
-
-.expired-header .text-danger {
-  color: #e53935;
-}
-.next-header .text-info {
-  color: #2196f3;
-}
-.fill-steps-header .text-primary {
-  color: #3f51b5;
-}
-.completed-header .text-success {
-  color: #4caf50;
-}
-
-.maintenance-items {
-  display: flex;
-  flex-direction: column;
-  gap: 0.75rem;
-  padding-bottom: 10px;
-  margin-left: 10px;
-  padding-right: 10px;
-}
-
-.maintenance-item {
-  background-color: #ffffff;
-  border-radius: 6px;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-}
-
-.maintenance-item.no-shadow {
-  box-shadow: none;
-}
-
-.maintenance-content {
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-}
-
-.maintenance-image-wrapper {
-  border-radius: 8px;
-  background: var(--Cores-Cinza-50, #eff3f5);
-  margin-right: 0.75rem;
-  display: flex;
-  padding: 5px;
-  align-items: center;
-  gap: 10px;
-}
-
-.maintenance-image {
-  width: 20px;
-  height: 20px;
-}
-
-.maintenance-details {
-  display: flex;
-  flex-direction: column;
-}
-
-.maintenance-name {
-  color: var(--Cores-Cinza-800, #33373c);
-
-  font-family: var(--Tipo-Familia-Tag, Inter);
-  font-size: var(--Tipo-Tamanho-Sm, 14px);
-  font-style: normal;
-  font-weight: 500;
-  line-height: 120%;
-}
-
-.maintenance-date {
-  align-self: stretch;
-  color: var(--Cores-Cinza-400, #76828b);
-
-  font-family: var(--Tipo-Familia-Tag, Inter);
-  font-size: var(--Tipo-Tamanho-Xs, 12px);
-  font-style: normal;
-  font-weight: 500;
-  line-height: 120%;
-}
-
-.maintenance-progress {
-  color: var(--Cores-Cinza-300, #9ba7ad);
-  font-family: var(--Tipo-Familia-Tag, Inter);
-  font-size: 10px;
-  font-style: normal;
-  font-weight: 500;
-  line-height: 120%;
+  gap: 0.5rem;
 }
 </style>
