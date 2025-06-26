8c0ec09802eeee168d2a1f373fb06632ec5f839e
yuri amaral santos <113852509+Yuri-amaralsantos@users.noreply.github.com>
2025-06-26 20:14:51 -0300

    Refactor: add parameters for buttons components

--- src/shared/components/ButtonComponent.vue
+++ src/shared/components/ButtonComponent.vue
@@ -1,38 +1,106 @@
 <template>
-  <q-btn
-    :label="label"
-    :loading="isLoading"
-    :disable="isLoading"
-    @click="handleButtonClick"
-    class="styled-button"
-    unelevated
-  />
+  <button
+    :class="['custom-button', sizeClass, colorClass]"
+    :disabled="isDisabled"
+    @click="handleClick"
+  >
+    <slot />
+  </button>
 </template>
 
 <script setup>
-const { label, isLoading } = defineProps({
-  label: String,
-  isLoading: Boolean,
+import { computed } from 'vue';
+
+const props = defineProps({
+  color: {
+    type: String,
+    default: 'default',
+    validator: (val) =>
+      ['default', 'secondary', 'outline', 'danger'].includes(val),
+  },
+  size: {
+    type: String,
+    default: 'default',
+    validator: (val) => ['large', 'medium', 'small'].includes(val),
+  },
+  isDisabled: {
+    type: Boolean,
+    default: false,
+  },
 });
 
-const emits = defineEmits(['click']);
+const emit = defineEmits(['click']);
 
-const handleButtonClick = () => {
-  emits('click');
+const handleClick = () => {
+  if (!props.isDisabled) {
+    emit('click');
+  }
 };
+
+const sizeClass = computed(() => `btn-size-${props.size}`);
+const colorClass = computed(() => `btn-color-${props.color}`);
 </script>
 
 <style scoped>
-.styled-button {
-  display: flex;
-  justify-content: center;
+.custom-button {
+  display: inline-flex;
   align-items: center;
+  justify-content: center;
+  font-weight: 600;
+  cursor: pointer;
+  border-radius: 8px;
+  white-space: nowrap;
   padding: 12px 24px;
-  border-radius: 4px;
-  background-color: #8ce95f;
-  color: #314b39;
-  font-weight: 700;
-  text-transform: capitalize;
-  font-size: 18px;
+  gap: 12px;
+}
+
+.custom-button:disabled {
+  background-color: #e0e5e7;
+  border: 1px solid #e0e5e7;
+  color: #485159;
+}
+
+/* Tamanhos */
+.btn-size-large {
+  font-size: 1.125rem;
+  height: 3.5rem;
+}
+
+.btn-size-medium {
+  font-size: 1rem;
+  height: 3rem;
+}
+
+.btn-size-small {
+  font-size: 0.938rem;
+  height: 2.5rem;
+}
+
+/* Cores */
+.btn-color-primary {
+  background-color: #307714;
+  color: white;
+  border: 1px solid #307714;
+}
+
+.btn-color-secondary {
+  background-color: #ffffff;
+  color: #307714;
+  border: 2px solid #307714;
+}
+
+.btn-color-tertiary {
+  background-color: #485159;
+  color: white;
+  border: 1px solid #485159;
+}
+
+.btn-color-atention {
+  background-color: #ed4647;
+  color: white;
+  border: 1px solid #ed4647;
+}
+
+.btn-color-inactive {
 }
 </style>
