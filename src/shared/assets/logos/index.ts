2fe51b194db3bd9d2f865fa15edc7399ad6327bc
Ricardo Magalhães <29683600+AmodeusR@users.noreply.github.com>
2025-07-01 18:25:45 -0300

    feat: add logos

---
+++ src/shared/assets/logos/index.ts
@@ -0,0 +1,4 @@
+export { default as Logo } from './logo.svg';
+export { default as LogoOneLine } from './logo-oneline.svg';
+export { default as LogoTwoLines } from './logo-twolines.svg';
+export { default as LogoTextVr } from './logo-text-vr.svg';
