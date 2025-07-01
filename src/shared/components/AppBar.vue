bc6e620963bb226e9f5170152193a17b984dcb32
Ricardo Magalhães <29683600+AmodeusR@users.noreply.github.com>
2025-07-01 18:27:55 -0300

    feat: create AppBar

---
+++ src/shared/components/AppBar.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import { RouterLink } from 'vue-router';
+import { BellIcon, ProfileIcon } from '../assets/icons';
+import { LogoOneLine } from '../assets/logos';
+
+
+</script>
+
+<template>
+  <header class="appbar">
+    <RouterLink to="/profile">
+      <img :src="ProfileIcon" alt="">
+    </RouterLink>
+    <img :src="LogoOneLine" alt="">
+    <RouterLink to="/notifications">
+      <img :src="BellIcon" alt="" class="appbar__icon">
+    </RouterLink>
+  </header>
+</template>
+
+
+
+<style scoped lang="scss">
+.appbar {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding-inline: 1.25rem;
+
+  min-height: 48px;
+  background-color: #fff;
+  border-bottom: 1px solid #E0E5E7;
+}
+</style>
