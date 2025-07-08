45532c38ef17c7d078fe7b00fe1fe030398200c1
Ricardo Magalhães <29683600+AmodeusR@users.noreply.github.com>
2025-07-07 23:01:27 -0300

    refactor: reorganize routes (order & nesting)

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -1,6 +1,6 @@
 import { h } from 'vue';
 import { RouteRecordRaw, RouterView } from 'vue-router';
-import { ROUTES } from './paths';
+// import { ROUTES } from './paths';
 
 const routes: RouteRecordRaw[] = [
   {
@@ -46,15 +46,6 @@ const routes: RouteRecordRaw[] = [
         path: '',
         component: () => import('src/layouts/AppLayout.vue'),
         children: [
-          {
-            path: 'notifications',
-            meta: {
-              title: 'Notificações',
-            },
-            component: () =>
-              import('@/pages/Notifications/NotificationPage.vue'),
-          },
-
           {
             path: 'home',
             component: () => import('@/pages/Home/HomePage.vue'),
@@ -110,91 +101,11 @@ const routes: RouteRecordRaw[] = [
             ],
           },
           {
-            path: 'registration-vehicle',
-            component: () => import('@/pages/Vehicle/RegistrationVehicle.vue'),
-          },
-          {
             path: 'history',
             component: () => import('@/pages/History/HistoryPage.vue'),
           },
           {
-            path: 'profile',
-            component: { render: () => h(RouterView) },
-            children: [
-              {
-                path: '',
-                component: () => import('@/pages/Profile/ProfilePage.vue'),
-              },
-              {
-                path: 'edit-name',
-                component: () =>
-                  import('@/pages/Profile/MyProfile/components/EditName.vue'),
-              },
-              {
-                path: 'edit-birthdate',
-                component: () =>
-                  import(
-                    '@/pages/Profile/MyProfile/components/EditBirthdate.vue'
-                  ),
-              },
-              {
-                path: 'edit-email',
-                component: () =>
-                  import('@/pages/Profile/MyProfile/components/EditEmail.vue'),
-              },
-              {
-                path: 'edit-password',
-                component: () =>
-                  import(
-                    '@/pages/Profile/MyProfile/components/EditPassword.vue'
-                  ),
-              },
-              {
-                path: 'terms-and-privacy',
-                component: () =>
-                  import(
-                    '@/pages/Profile/MyProfile/components/TermsAndPrivacy.vue'
-                  ),
-              },
-              {
-                path: 'edit-model',
-                component: () =>
-                  import(
-                    '@/pages/Profile/MyVehicle/components/VehicleEdit/components/EditModel.vue'
-                  ),
-              },
-              {
-                path: 'edit-brand',
-                component: () =>
-                  import(
-                    '@/pages/Profile/MyVehicle/components/VehicleEdit/components/EditBrand.vue'
-                  ),
-              },
-              {
-                path: 'edit-year',
-                component: () =>
-                  import(
-                    '@/pages/Profile/MyVehicle/components/VehicleEdit/components/EditYear.vue'
-                  ),
-              },
-              {
-                path: 'edit-plate',
-                component: () =>
-                  import(
-                    '@/pages/Profile/MyVehicle/components/VehicleEdit/components/EditPlate.vue'
-                  ),
-              },
-              {
-                path: 'edit-color',
-                component: () =>
-                  import(
-                    '@/pages/Profile/MyVehicle/components/VehicleEdit/components/EditColor.vue'
-                  ),
-              },
-            ],
-          },
-          {
-            path: ROUTES.FINANCES.ROOT,
+            path: 'finances',
             component: { render: () => h(RouterView) },
             meta: {
               title: 'Finanças',
@@ -208,7 +119,7 @@ const routes: RouteRecordRaw[] = [
                 },
               },
               {
-                path: ROUTES.FINANCES.STORE_MONEY,
+                path: 'store-money',
                 component: () =>
                   import('@/pages/Finances/components/StoreMoney.vue'),
               },
@@ -218,6 +129,123 @@ const routes: RouteRecordRaw[] = [
                   import('@/pages/Finances/components/EditValue.vue'),
               },
             ],
+          },
+          {
+            path: 'user',
+            component: { render: () => h(RouterView) },
+            children: [
+              {
+                path: '',
+                component: () => import('@/pages/Profile/ProfilePage.vue'),
+              },
+              {
+                path: 'profile',
+                component: { render: () => h(RouterView) },
+                meta: {
+                  title: 'Editar perfil',
+                },
+                children: [
+                  {
+                    path: 'edit-name',
+                    component: () =>
+                      import(
+                        '@/pages/Profile/MyProfile/components/EditName.vue'
+                      ),
+                  },
+                  {
+                    path: 'edit-birthdate',
+                    component: () =>
+                      import(
+                        '@/pages/Profile/MyProfile/components/EditBirthdate.vue'
+                      ),
+                  },
+                  {
+                    path: 'edit-email',
+                    component: () =>
+                      import(
+                        '@/pages/Profile/MyProfile/components/EditEmail.vue'
+                      ),
+                  },
+                  {
+                    path: 'edit-password',
+                    component: () =>
+                      import(
+                        '@/pages/Profile/MyProfile/components/EditPassword.vue'
+                      ),
+                  },
+                  {
+                    path: 'terms-and-privacy',
+                    component: () =>
+                      import(
+                        '@/pages/Profile/MyProfile/components/TermsAndPrivacy.vue'
+                      ),
+                  },
+                ],
+              },
+              {
+                path: 'vehicle',
+                component: { render: () => h(RouterView) },
+                meta: {
+                  title: 'Editar veículo',
+                },
+                children: [
+                  {
+                    path: '',
+                    component: () =>
+                      import('@/pages/Profile/MyVehicle/MyVehicle.vue'),
+                    children: [],
+                  },
+                  {
+                    path: 'edit-model',
+                    component: () =>
+                      import(
+                        '@/pages/Profile/MyVehicle/components/VehicleEdit/components/EditModel.vue'
+                      ),
+                  },
+                  {
+                    path: 'edit-brand',
+                    component: () =>
+                      import(
+                        '@/pages/Profile/MyVehicle/components/VehicleEdit/components/EditBrand.vue'
+                      ),
+                  },
+                  {
+                    path: 'edit-year',
+                    component: () =>
+                      import(
+                        '@/pages/Profile/MyVehicle/components/VehicleEdit/components/EditYear.vue'
+                      ),
+                  },
+                  {
+                    path: 'edit-plate',
+                    component: () =>
+                      import(
+                        '@/pages/Profile/MyVehicle/components/VehicleEdit/components/EditPlate.vue'
+                      ),
+                  },
+                  {
+                    path: 'edit-color',
+                    component: () =>
+                      import(
+                        '@/pages/Profile/MyVehicle/components/VehicleEdit/components/EditColor.vue'
+                      ),
+                  },
+                  {
+                    path: 'registration-vehicle',
+                    component: () =>
+                      import('@/pages/Vehicle/RegistrationVehicle.vue'),
+                  },
+                ],
+              },
+            ],
+          },
+          {
+            path: 'notifications',
+            meta: {
+              title: 'Notificações',
+            },
+            component: () =>
+              import('@/pages/Notifications/NotificationPage.vue'),
           },
         ],
       },
