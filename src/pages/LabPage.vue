d6d2c1cd705f30ad551f08da4ebeb31a1aa5507e
yuri amaral santos <113852509+Yuri-amaralsantos@users.noreply.github.com>
2025-07-26 13:25:30 -0300

    feat: add bottom sheet in lab page

--- src/pages/LabPage.vue
+++ src/pages/LabPage.vue
@@ -1,15 +1,36 @@
 <script setup lang="ts">
+import { ref } from 'vue';
 import CButton from '@/shared/components/CButton.vue';
+import CBottomSheet from '@/shared/components/CBottomSheet.vue';
 
-const action = () => {
-  console.log('Laboratório');
-};
+const showSheet = ref(false);
+const showSheet2 = ref(false);
+const showSheet3 = ref(false);
 </script>
 
 <template>
   <div class="lab app-wrapper">
-    <CButton @click="action">Clique aqui</CButton>
+    <CButton @click="showSheet = true">bottom-sheet1</CButton>
+
+    <CButton @click="showSheet2 = true">bottom-sheet2</CButton>
+
+    <CButton @click="showSheet3 = true">bottom-sheet3</CButton>
   </div>
+
+  <CBottomSheet v-model="showSheet" variant="default">
+    <p>tests</p>
+    <CButton @click="showSheet = false">Fechar</CButton>
+  </CBottomSheet>
+
+  <CBottomSheet v-model="showSheet2" variant="default">
+    <p>tests</p>
+    <CButton @click="showSheet3 = false">Fechar</CButton>
+  </CBottomSheet>
+
+  <CBottomSheet v-model="showSheet3" variant="default">
+    <p>tests</p>
+    <CButton @click="showSheet3 = false">Fechar</CButton>
+  </CBottomSheet>
 </template>
 
 <style scoped lang="scss">
