37bcdc484659a7c5c6ec7374a48db0efee77144d
yuri amaral santos <113852509+Yuri-amaralsantos@users.noreply.github.com>
2025-08-06 12:41:12 -0300

    feat: add component check box

---
+++ src/shared/components/CCheckBox.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+const isChecked = defineModel({ default: false });
+</script>
+
+<template>
+  <input class="check-box" type="checkbox" v-model="isChecked" />
+</template>
+
+<style scoped lang="scss">
+.check-box {
+  appearance: none;
+  border: 2px solid #485159;
+  border-radius: 4px;
+  width: 20px;
+  height: 20px;
+  cursor: pointer;
+  position: relative;
+  transition: background-color 0.2s, border-color 0.2s;
+
+  &:checked {
+    background-color: #307714;
+    border-color: #307714;
+  }
+
+  &:checked::after {
+    content: '';
+    position: absolute;
+    left: 5px;
+    top: 2px;
+    width: 6px;
+    height: 10px;
+    border: solid white;
+    border-width: 0 2px 2px 0;
+    transform: rotate(45deg);
+    border-radius: 1px;
+  }
+}
+</style>
