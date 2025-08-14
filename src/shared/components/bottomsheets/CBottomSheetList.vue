c9248f3f1fa7c3d9b654c1ee843eeed11031cae0
Ricardo Magalhães <29683600+AmodeusR@users.noreply.github.com>
2025-08-14 20:43:46 -0300

    feat: create alt bottom-sheet components

---
+++ src/shared/components/bottomsheets/CBottomSheetList.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { bottomSheetProps } from '@/shared/types/bottom-sheet';
+import CBottomSheet from './CBottomSheet.vue';
+
+const props = defineProps<
+  bottomSheetProps & {
+    type: 'filter' | 'action';
+  }
+>();
+</script>
+
+<template>
+  <CBottomSheet v-bind="{ ...props }">
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi,
+      deleniti.
+    </p>
+    <p>
+      Esse dolores eos ipsa quos doloribus sapiente labore magnam dignissimos.
+    </p>
+  </CBottomSheet>
+</template>
+
+<style scoped lang="scss"></style>
---
+++ src/shared/components/bottomsheets/CBottomSheetText.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div>
+    <h2>This is a text bottom sheet</h2>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maiores, tempore.</p>
+  </div>
+</template>
+
+
+
+<style scoped lang="scss">
+
+</style>
