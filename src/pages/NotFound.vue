117fa0fd42c3d3280ff0b0429f759e9c865ab00e
yuri amaral santos <113852509+Yuri-amaralsantos@users.noreply.github.com>
2025-09-21 22:03:05 -0300

    feat(error): change div to router link, change image name, change fonts and paddings

--- src/pages/NotFound.vue
+++ src/pages/NotFound.vue
@@ -1,28 +1,24 @@
 <script setup lang="ts">
-import { useRouter } from 'vue-router';
+import { RouterLink } from 'vue-router';
 import { ArrowIcon } from '@/shared/assets/icons';
 import CButton from '@/shared/components/CButton.vue';
-import mecanicoImage from '../shared/assets/illustrations/mecanico-duvida.png';
-
-const router = useRouter();
-
-const navigateBack = () => {
-  router.push('/');
-};
+import mecanicoImage from '../shared/assets/illustrations/mechanic-not-found.png';
 </script>
 
 <template>
   <div class="error-page app-wrapper">
-    <div @click="navigateBack" class="return-button">
+    <RouterLink :to="{ name: 'home' }" class="return-button">
       <q-img :src="ArrowIcon" />
-    </div>
+    </RouterLink>
 
     <img :src="mecanicoImage" />
     <div class="text">
       <h2>Erro 404</h2>
       <p>Não conseguimos encontrar a página que você está procurando</p>
     </div>
-    <CButton variant="primary" :to="{ name: 'home' }"> Início </CButton>
+    <CButton variant="primary" size="large" :to="{ name: 'home' }">
+      Início
+    </CButton>
   </div>
 </template>
 
@@ -38,22 +34,29 @@ const navigateBack = () => {
 
 .text {
   text-align: center;
+  padding: 0 30px;
   h2 {
     font-weight: 700;
-    font-size: 1.25;
+    font-size: 1.25rem;
     padding-bottom: 0.5rem;
     line-height: 120%;
   }
 
   p {
     line-height: 150%;
+    color: #485159;
   }
 }
 
 .return-button {
-  width: 24px;
-  height: 24px;
-  cursor: pointer;
+  padding-left: 10px;
+  width: 26px;
+  height: 26px;
+
   transform: rotate(-90deg);
+  .q-img {
+    width: 100%;
+    height: 100%;
+  }
 }
 </style>
