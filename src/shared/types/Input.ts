b42c64e62d9f15a2895a8cd132bc1857ac24e0e7
Ricardo Magalhães <29683600+AmodeusR@users.noreply.github.com>
2025-09-23 13:51:15 -0300

    feat: add variant

--- src/shared/types/Input.ts
+++ src/shared/types/Input.ts
@@ -2,10 +2,12 @@ import { InputHTMLAttributes } from 'vue';
 
 export type Input = {
   label?: string;
+  name: string;
   placeholder?: string;
   icon?: string;
   showIcon?: 'always' | 'not-empty';
   disabled?: boolean;
   supportingText?: string;
   action?: () => void;
-} & /* @vue-ignore */  InputHTMLAttributes;
+  variant: 'text' | 'password'
+} & /* @vue-ignore */ InputHTMLAttributes;
