a4c36843f31fda31074e97530b969b8114ad226b
Ricardo Magalhães <29683600+AmodeusR@users.noreply.github.com>
2025-09-25 12:10:00 -0300

    update lab

--- src/pages/LabPage.vue
+++ src/pages/LabPage.vue
@@ -1,23 +1,50 @@
 <script setup lang="ts">
 import CButton from '@/shared/components/CButton.vue';
-import { XCircleIcon } from '@/shared/assets/icons';
-import { ref } from 'vue';
+import { ref, watch } from 'vue';
 import CInput from '@/shared/components/CInput.vue';
+import { useForm } from 'vee-validate';
+import { z } from 'zod';
+import { toTypedSchema } from '@vee-validate/zod';
 
 const allInput = ref('');
+
+const schema = z.object({
+  name: z.string().min(5, 'O nome precisa ter ao menos 5 caracteres'),
+  email: z.string().email()
+});
+
+const { handleSubmit, errors, meta } = useForm({
+  validationSchema: toTypedSchema(schema)
+});
+
+const onSubmit = handleSubmit(values => {
+  console.log(values);
+});
+
+watch(errors, () => {
+  console.log(meta.value.valid);
+})
+
 </script>
 
 <template>
   <div class="lab app-wrapper">
-    <CInput
-      label="nome"
-      name="teste"
-      v-model="allInput"
-      variant="text"
-      :icon="XCircleIcon"
-    />
-
-    <CButton>Clique aqui</CButton>
+    <form @submit="onSubmit" class="form">
+      <CInput
+        label="nome"
+        name="name"
+        v-model="allInput"
+        variant="text"
+      />
+      <CInput
+        label="email"
+        name="email"
+        v-model="allInput"
+        variant="text"
+        type="email"
+      />
+      <CButton type="submit" :disabled="!meta.valid">Clique aqui</CButton>
+    </form>
   </div>
 </template>
 
@@ -29,5 +56,10 @@ const allInput = ref('');
   align-content: center;
   gap: 1rem;
   min-height: $screen;
+}
+
+.form {
+  display: grid;
+  gap: 1rem;
 }
 </style>
