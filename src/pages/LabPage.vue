2122c40c6d10dfdcd9ca93a8c0653f78e5fb415b
Ricardo Magalhães <29683600+AmodeusR@users.noreply.github.com>
2025-10-01 10:26:08 -0300

    lab

--- src/pages/LabPage.vue
+++ src/pages/LabPage.vue
@@ -1,47 +1,45 @@
 <script setup lang="ts">
 import CButton from '@/shared/components/CButton.vue';
-import { ref, watch } from 'vue';
 import CInput from '@/shared/components/CInput.vue';
 import { useForm } from 'vee-validate';
-import { z } from 'zod';
-import { toTypedSchema } from '@vee-validate/zod';
+import { vMaska } from 'maska/vue';
+import type { MaskInputOptions } from 'maska';
+import * as yup from 'yup';
 
-const allInput = ref('');
+const options: MaskInputOptions = {
+  mask: ['AAA-####', 'AAA#A##'],
+  tokens: {
+    A: {
+      pattern: /[A-Z]/,
+      transform: (char: string) => char.toUpperCase()
+    }
+  }
+}
 
-const schema = z.object({
-  name: z.string().min(5, 'O nome precisa ter ao menos 5 caracteres'),
-  email: z.string().email()
+const { handleSubmit, meta } = useForm({
+  validationSchema: yup.object({
+    name: yup.string().min(5, 'O nome precisa ter 5 ou mais letras').required(),
+    email: yup.string().email().required(),
+    plate: yup.string().length(8, 'A placa do carro deve ter 7 caracteres')
+  }),
 });
 
-const { handleSubmit, errors, meta } = useForm({
-  validationSchema: toTypedSchema(schema)
-});
-
-const onSubmit = handleSubmit(values => {
+const onSubmit = handleSubmit((values) => {
   console.log(values);
 });
-
-watch(errors, () => {
-  console.log(meta.value.valid);
-})
-
 </script>
 
 <template>
   <div class="lab app-wrapper">
     <form @submit="onSubmit" class="form">
+      <CInput label="nome" name="name" variant="generic" />
+      <CInput label="email" name="email" variant="generic" type="email" />
       <CInput
-        label="nome"
-        name="name"
-        v-model="allInput"
-        variant="text"
-      />
-      <CInput
-        label="email"
-        name="email"
-        v-model="allInput"
-        variant="text"
-        type="email"
+        label="Placa do carro"
+        name="plate"
+        variant="generic"
+        type="text"
+        v-maska="options"
       />
       <CButton type="submit" :disabled="!meta.valid">Clique aqui</CButton>
     </form>
